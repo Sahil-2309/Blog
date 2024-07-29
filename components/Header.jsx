8471f7ac3402bfeec22650c6540d6e5bbb1ec238
Sahil Arora <91737464+Sahil-2309@users.noreply.github.com>
2024-07-30 02:14:14 +0530

    named cloud learn

--- components/Header.jsx
+++ components/Header.jsx
@@ -22,7 +22,7 @@ const Header = () => {
         <div className='md:float-left block'>
           <Link href='/'>
             <span className='cursor-pointer font-bold text-4xl text-white'>
-              GraphCMS
+              CloudLearn
             </span>
           </Link>
         </div>
